61c1e50e2fe8df11507ac68576d0332c5470fa8e
Edilson Gomes <edilson.gs1602@gmail.com>
2023-09-01 12:24:00 -0300

    feat: insert unit test in cityService

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,9 +10,6 @@ import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { RolesGuard } from './guards/roles.guard';
 import { JwtModule } from '@nestjs/jwt';
-import { UserService } from './user/user.service';
-import { CityService } from './city/city.service';
-import { StateService } from './state/state.service';
 
 @Module({
   imports: [
@@ -44,9 +41,6 @@ import { StateService } from './state/state.service';
       provide: APP_GUARD,
       useClass: RolesGuard,
     },
-    UserService,
-    CityService,
-    StateService,
   ],
 })
 export class AppModule {}
