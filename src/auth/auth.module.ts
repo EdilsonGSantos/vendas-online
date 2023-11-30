ae267d4cabdd9c3cadb1fd126451cdfb84ce16f6
Edilson Gomes <edilson.gs1602@gmail.com>
2023-11-30 13:19:42 -0300

    system update

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,17 +2,18 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
-import { JwtModule } from '@nestjs/jwt';
+import { JwtModule, JwtModuleAsyncOptions } from '@nestjs/jwt';
 
 @Module({
   imports: [
     UserModule,
     JwtModule.registerAsync({
+      imports: [UserModule],
       useFactory: () => ({
         secret: process.env.JWT_SECRET,
         signOptions: { expiresIn: process.env.JWT_EXPIRES_IN },
       }),
-    }),
+    } as JwtModuleAsyncOptions),
   ],
   providers: [AuthService],
   controllers: [AuthController],
